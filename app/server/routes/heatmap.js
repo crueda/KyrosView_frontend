ef0a83cdf93d53f8418d55df66bba3dce96fb216
Carlos Rueda Morales <carlos.rueda@deimos-space.com>
2016-10-04 10:10:01 +0200

    The same thing we do every night, Pinky - try to take over the world!

---
+++ app/server/routes/heatmap.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var router = express.Router();
+var jsonfy = require('jsonfy');
+var moment = require('moment');
+
+var status = require("../utils/statusCodes.js");
+var messages = require("../utils/statusMessages.js");
+
+// Fichero de propiedades
+var PropertiesReader = require('properties-reader');
+var properties = PropertiesReader('./kyrosview.properties');
+
+var HeatmapModel = require('../models/heatmap');
+
+// Definición del log
+var fs = require('fs');
+var log = require('tracer').console({
+  transport : function(data) {
+    //console.log(data.output);
+    fs.open(properties.get('main.log.file'), 'a', 0666, function(e, id) {
+      fs.write(id, data.output+"\n", null, 'utf8', function() {
+        fs.close(id, function() {
+        });
+      });
+    });
+  }
+});
+
+router.get('/heatmap', function(req, res)
+{
+    if (req.session.user == null){
+      res.redirect('/');
+    } 
+    else {
+      var deviceIdList = req.query.deviceIdList;
+      var initDate = req.query.initDate;
+      var endDate = req.query.endDate;
+
+      log.info("GET: /heatmap/"+deviceId);
+
+      if (endDate==null) {
+        endDate = (new Date).getTime()
+      }
+
+      if (initDate==null) {
+        res.status(202).json({"response": {"status":status.STATUS_VALIDATION_ERROR,"description":messages.MISSING_PARAMETER}})
+      } 
+      else {
+        var requestData = {
+          deviceIdList : deviceIdList,
+          initDate : initDate,
+          endDate : endDate
+        };
+        HeatmapModel.getHeatmapData(requestData, function(error, data)
+        {
+          if (data == null)
+          {
+            res.status(202).json({"response": {"status":status.STATUS_FAILURE,"description":messages.DB_ERROR}})
+          }
+          else
+          {
+            //si existe enviamos el json
+            if (typeof data !== 'undefined' && data.length > 0)
+            {
+              res.status(200).json(jsonfy(data))
+            }
+            else if (typeof data == 'undefined' || data.length == 0)
+            {
+              res.status(200).json([])
+            }
+            //en otro caso mostramos un error
+            else
+            {
+              res.status(202).json({"response": {"status":status.STATUS_NOT_FOUND_REGISTER,"description":messages.MISSING_REGISTER}})
+            }
+          }
+        }); 
+      }   
+    }
+});
+
+module.exports = router;
