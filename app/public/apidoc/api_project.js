5f71cb16d0c74b99f8b56b02e91d02228276ca8d
Carlos Rueda Morales <carlos.rueda@deimos-space.com>
2016-10-06 18:13:51 +0200

    what the hell happened here

--- app/public/apidoc/api_project.js
+++ app/public/apidoc/api_project.js
@@ -1,1 +1,1 @@
-define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-10-05T16:45:40.671Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
+define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-10-06T15:12:27.959Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
