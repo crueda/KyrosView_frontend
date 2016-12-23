d30a3274c18af2810a361cd2004c684c9d6801d9
Carlos Rueda Morales <carlos.rueda@deimos-space.com>
2016-12-23 13:51:07 +0100

    Fucking submodule bull shit

---
+++ app/public/js/kyrosview/constants.js
@@ -0,0 +1,6 @@
+ var Constants = {
+  'url_images': "https://images.kyroslbs.com/kyrosView/images/",
+  'url_img': "https://images.kyroslbs.com/kyrosView/img/",
+  'url_events': "https://images.kyroslbs.com/kyrosView/events/"
+};
+
